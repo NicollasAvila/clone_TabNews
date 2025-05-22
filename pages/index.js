7e45bf5e06e954769227e208eb9561b72ffaa45c
NicollasAvila <112048482+NicollasAvila@users.noreply.github.com>
2025-05-22 10:42:11 -0300

    voltando pg

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ export default function Home() {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Bem-vindo ao Meu Site! <br/>
-                Eai Calebe
             </h1>
             <p>Este é um projeto simples criado com Next.js.</p>
             <button style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}>
